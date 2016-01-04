820e1694fb6c3d04f639ecf060af2633a81ae5a2
Brendan Shephard <shep@Sheps-MacBook-Pro.local>
2016-01-04 23:38:52 +1000

    Added some comments

--- Day3.py
+++ Day3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+# This function will determine whether the N variable will meet the specifications of the task. Laziness 
+# has prevented me from typing them all in here.
 
 def weird(N):
     if N % 2 == 0 and 1 <= N <= 20 or N % 2 == 1 and 20 <= N:
@@ -8,6 +10,8 @@ def weird(N):
         i = "Weird"
 
     return i
+
+# Get N from the user. Send it off to weird() for processing and await a reply.
 
 def main():
 
